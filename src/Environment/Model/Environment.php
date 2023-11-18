72f2a96d31dbaad7f64ae6ec348a1eda9b51148d
Iqbal Maulana <iq.bluejack@gmail.com>
2023-11-18 23:06:35 +0700

    fix: remove api resource for environment

--- src/Environment/Model/Environment.php
+++ src/Environment/Model/Environment.php
@@ -5,39 +5,10 @@ declare(strict_types=1);
 namespace Bima\Environment\Model;
 
 use Bima\Instance\Model\Instance;
-use Pandawa\Annotations\Resource\ApiResource;
 use Pandawa\Component\Eloquent\Model;
 use Pandawa\Component\Eloquent\Relations\HasMany;
 
-/**
- * @author  Iqbal Maulana <iq.bluejack@gmail.com>
- */
-#[ApiResource(
-    uri: 'environments',
-    routeGroup: 'api',
-    middleware: ['auth'],
-    except: ['delete'],
-    options: [
-        'store' => [
-            'rules' => [
-                [
-                    'constraints' => [
-                        'name' => 'required|string',
-                    ]
-                ]
-            ]
-        ],
-        'update' => [
-            'rules' => [
-                [
-                    'constraints' => [
-                        'name' => 'required|string',
-                    ]
-                ]
-            ]
-        ]
-    ]
-)]
+
 class Environment extends Model
 {
     protected $appends = ['instances_count'];
